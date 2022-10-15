6b961de09016839eb283b8f9f8967902cf46eb1b
Se Jin Lee <sjleesogang@gmail.com>
2022-10-15 17:54:58 -0400

    Time: 556 ms (81.38%), Space: 15.5 MB (24.37%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,24 +1,15 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        if len(piles) == h :
-            return max(piles)
         left = 1 
         right = max(piles)
-        ans = float('inf')
-        while left <= right :
+        while left < right :
             mid = (left + right)//2
             hr = 0
             for i in range(len(piles)) :
-                if mid > piles[i] :
-                    hr += 1
-                else :
-                    hr += piles[i]//mid
-                    if piles[i] % mid > 0 :
-                        hr+=1 
+                hr += (piles[i] // mid) + (piles[i]%mid != 0 )
             if hr <= h :
-                ans = min(ans,mid)
-                right = mid-1
+                right = mid
             else :
                 left = mid+1
         
-        return ans
+        return right
