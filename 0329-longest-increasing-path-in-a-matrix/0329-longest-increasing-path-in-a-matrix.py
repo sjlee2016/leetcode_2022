233c3471962c3d200e72ed41d3ad0fdef3cbaa61
Se Jin Lee <sjleesogang@gmail.com>
2022-10-15 01:44:08 -0400

    Time: 1974 ms (5.01%), Space: 15 MB (75.28%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,25 +1,20 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        
-        dp = {}
+        dp = [ [0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
         def dfs(r,c) : 
             nonlocal dp
-            if (r,c) in dp :
-                return dp[(r,c)]
-            
+            if dp[r][c] != 0 :
+                return dp[r][c] 
+              
             for d in [[1,0],[-1,0],[0,1],[0,-1]] :
                 newR = r+d[0]
                 newC = c+d[1]
                 if newR >= 0 and newC >= 0 and newR < len(matrix) and newC < len(matrix[0]) and matrix[newR][newC] > matrix[r][c] :
-                    if (r,c) in dp :
-                        dp[(r,c)] = max(dp[(r,c)], dfs(newR,newC)+1) 
-                    else :
-                        dp[(r,c)] = dfs(newR,newC)+1
-            if (r,c) not in dp :
-                dp[(r,c)] = 1 
-            return dp[(r,c)]
+                    dp[r][c] = max(dp[r][c], dfs(newR,newC)) 
+            dp[r][c]+= 1     
+            return dp[r][c]
         
-        ans = 1
+        ans = 0
         for i in range(len(matrix)) :
             for j in range(len(matrix[0])) :
                 ans = max(ans,dfs(i,j))
