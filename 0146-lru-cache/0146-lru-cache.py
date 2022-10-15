e5cf2d8dfb3e2740b1dd20bb257d9d971a4a21e3
Se Jin Lee <sjleesogang@gmail.com>
2022-10-14 23:08:01 -0400

    Time: 2466 ms (9.96%), Space: 77.1 MB (10.20%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,22 +1,63 @@
-from collections import OrderedDict
-class LRUCache(OrderedDict):
-
+class Node() :
+    def __init(self) :
+        self.key = 0
+        self.value = 0
+        self.prev = None 
+        self.next = None 
+        
+class LRUCache:
+    def _add_node(self,node) :
+        node.prev = self.head 
+        node.next = self.head.next 
+        self.head.next.prev = node 
+        self.head.next = node
+        
+    def _pop_node(self,node) :
+        prev = node.prev 
+        new = node.next 
+        prev.next = new 
+        new.prev = prev 
+    
+    def _move_to_head(self,node) :
+        self._pop_node(node)
+        self._add_node(node)
+    
+    def _pop_tail(self) :
+        n = self.tail.prev
+        self._pop_node(n)
+        return n
     def __init__(self, capacity: int):
         self.capacity = capacity
+        self.cache = {}
+        self.size = 0
+        self.head, self.tail = Node(), Node()
+        self.head.next = self.tail 
+        self.tail.prev = self.head 
+        
 
     def get(self, key: int) -> int:
-        if key not in self :
-            return -1
-        self.move_to_end(key)
-        return self[key]
-
+        if key in self.cache :
+            self._move_to_head(self.cache[key])
+            return self.cache[key].value
+        else :
+            return -1 
     def put(self, key: int, value: int) -> None:
-        if key in self :
-            self.move_to_end(key)
-        self[key] = value
-        if len(self) > self.capacity :
-            self.popitem(last = False)
-
+        if key in self.cache :
+            self.cache[key].value = value
+            self._move_to_head(self.cache[key])
+        else :
+            newNode = Node()
+            newNode.value = value
+            newNode.key = key
+            
+            self.cache[key] = newNode
+            self._add_node(newNode)
+            self.size += 1
+            
+            if self.size > self.capacity :
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
