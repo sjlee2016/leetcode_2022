8b620094d924d1087725a9a148a2eea77eb89b56
Se Jin Lee <sjleesogang@gmail.com>
2022-10-17 15:57:56 -0400

    Time: 32 ms (33.05%), Space: 13.4 MB (62.53%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -5,32 +5,23 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: List[str]
         """
-        if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
-            return []
-
         wordSet = set(wordDict)
-
-        dp = [[]] * (len(s)+1)
-        dp[0] = [[0]]
-
-        for endIndex in range(1, len(s)+1):
-            stops = []
-            for startIndex in range(0, endIndex):
-                word = s[startIndex:endIndex]
-                if word in wordSet:
-                    stops.append([startIndex, endIndex])
-            dp[endIndex] = stops
-
-        ret = []
-        def wordDFS(sentence, dp_index):
-            if dp_index == 0:
-                ret.append(" ".join(sentence))
-                return
-
-            for start, end in dp[dp_index]:
-                word = s[start:end]
-                wordDFS([word] + sentence, start)
-
-        wordDFS([], len(s))
-
-        return ret
+        memo = defaultdict(list)
+        
+        #@lru_cache(maxsize=None)
+        def _wordBreak_topdown(s) :
+            if not s :
+                return [[]]
+            if s in memo :
+                return memo[s] 
+            
+            for endIndex in range(1, len(s)+1):
+                word = s[:endIndex]
+                if word in wordSet :
+                    for subsentence in _wordBreak_topdown(s[endIndex:])  :
+                        memo[s].append([word]+ subsentence)
+            return memo[s]
+        
+        _wordBreak_topdown(s)
+        
+        return [" ".join(words) for words in memo[s]]
