189e6951d3ce0aaf57d782ad01ea7c7c73d68053
Se Jin Lee <sjleesogang@gmail.com>
2022-10-17 15:41:08 -0400

    Time: 47 ms (50.09%), Space: 13.5 MB (89.11%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        wordSet = set(wordDict)
+        
+        dp = [False] * (len(s) + 1) 
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1 ) :
+            for j in range(i) :
+                if dp[j] and s[j:i] in wordSet :
+                    dp[i] = True
+                    break
+        return dp[len(s)]
