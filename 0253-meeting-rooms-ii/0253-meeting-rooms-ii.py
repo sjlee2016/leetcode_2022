5b8db1560c23e55cec25e6c085df851c1a6b8c29
Se Jin Lee <sjleesogang@gmail.com>
2022-10-17 18:00:01 -0400

    Time: 81 ms (74.86%), Space: 17.1 MB (23.33%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        if not intervals :
+            return 0 
+        
+        free_rooms = [] 
+        
+        intervals = sorted(intervals, key = lambda x : x[0])
+        
+        heapq.heappush(free_rooms, intervals[0][1]) 
+        
+        
+        for i in intervals[1:] : 
+            if free_rooms[0] <= i[0] :
+                heapq.heappop(free_rooms)
+            
+            
+            heapq.heappush(free_rooms, i[1])
+        
+        
+        return len(free_rooms)
