88091f0bbc157c06588d4e6546e8283a5c2943f5
Se Jin Lee <sjleesogang@gmail.com>
2022-10-18 00:19:32 -0400

    Time: 471 ms (47.53%), Space: 27.3 MB (17.49%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,22 +1,38 @@
-class UndergroundSystem:
+class UndergroundSystem(object):
 
     def __init__(self):
-        self.check_in = {}
-        self.time_taken = {} 
+        self.checkInHistory = {}
+        self.travelLogs = {} 
+    def checkIn(self, id, stationName, t):
+        """
+        :type id: int
+        :type stationName: str
+        :type t: int
+        :rtype: None
+        """
+        self.checkInHistory[id] = (stationName,t)
         
-    def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.check_in[id] = (stationName, t)
 
-    def checkOut(self, id: int, stationName: str, t: int) -> None:
-        startStation, startTime = self.check_in[id]
-        if (startStation, stationName) not in self.time_taken :
-            self.time_taken[(startStation,stationName)] = [t-startTime, 1]
+    def checkOut(self, id, stationName, t):
+        """
+        :type id: int
+        :type stationName: str
+        :type t: int
+        :rtype: None
+        """
+        (startStation, startTime ) = self.checkInHistory[id]
+        if (startStation,stationName) not in self.travelLogs :
+            self.travelLogs[(startStation,stationName)] = [t-startTime,1]
         else :
-            self.time_taken[(startStation,stationName)][0] += t-startTime
-            self.time_taken[(startStation,stationName)][1] += 1
-            
-    def getAverageTime(self, startStation: str, endStation: str) -> float:
-        return self.time_taken[(startStation,endStation)][0] / self.time_taken[(startStation,endStation)][1] 
+            self.travelLogs[(startStation,stationName)][0] += (t-startTime)
+            self.travelLogs[(startStation,stationName)][1] += 1
+    def getAverageTime(self, startStation, endStation):
+        """
+        :type startStation: str
+        :type endStation: str
+        :rtype: float
+        """
+        return (1.0 * self.travelLogs[(startStation,endStation)][0]) / self.travelLogs[(startStation,endStation)][1]
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
